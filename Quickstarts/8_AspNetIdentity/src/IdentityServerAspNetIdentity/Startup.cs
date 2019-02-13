7057a51a4f2fd6e84b3f80c832993d2ca4ae7280
Phil@Djali <phil@ms1.hle.com.tw>
2019-02-13 17:07:48 +0800

    add google client id and secret

--- Quickstarts/8_AspNetIdentity/src/IdentityServerAspNetIdentity/Startup.cs
+++ Quickstarts/8_AspNetIdentity/src/IdentityServerAspNetIdentity/Startup.cs
@@ -66,11 +66,13 @@ namespace IdentityServerAspNetIdentity
             services.AddAuthentication()
                 .AddGoogle(options =>
                 {
+//                    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+
                     // register your IdentityServer with Google at https://console.developers.google.com
                     // enable the Google+ API
-                    // set the redirect URI to http://localhost:5000/signin-google
-                    options.ClientId = "copy client ID from Google here";
-                    options.ClientSecret = "copy client secret from Google here";
+                    // set the redirect URI to http://localhost:port/signin-google
+                    options.ClientId = "904877914910-i5o6769bend3vuba91lir7umevurip1f.apps.googleusercontent.com";
+                    options.ClientSecret = "Yq05i4fpnmALLPXItDMyXsGT";
                 });
         }
 
