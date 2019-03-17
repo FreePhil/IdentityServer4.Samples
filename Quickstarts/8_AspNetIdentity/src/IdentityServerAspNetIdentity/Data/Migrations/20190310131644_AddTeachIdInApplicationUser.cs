cd7a0f41e4f8605cf211f814b090fe11ac633872
Phil@Laverne <phil@ms1.hanlin.com.tw>
2019-03-17 17:24:30 +0800

    add corret google provider's secret

---
+++ Quickstarts/8_AspNetIdentity/src/IdentityServerAspNetIdentity/Data/Migrations/20190310131644_AddTeachIdInApplicationUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IdentityServerAspNetIdentity.Data.Migrations
+{
+    public partial class AddTeachIdInApplicationUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TeacherId",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TeacherId",
+                table: "AspNetUsers");
+        }
+    }
+}
